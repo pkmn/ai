ea8153a44147fa2db45e2be9dedc418115d83226
Kirk Scheibelhut <kjs@scheibo.com>
2023-11-03 21:31:33 -0700

    initial formatting for research page

--- src/site/research.ts
+++ src/site/research.ts
@@ -3,17 +3,25 @@ import * as path from 'path';
 
 import * as bibtex from '@retorquere/bibtex-parser';
 
-// TODO generate listing from research.bib (and tags) - link externally always
 export function page(dir: string) {
+  const buf: string[] = [];
+
   const bib = bibtex.parse(fs.readFileSync(path.join(dir, 'research.bib'), 'utf8'));
   if (bib.errors.length) throw new Error(`Error parsing research.bib: ${bib.errors.join(', ')}`);
-  const bibliography: {[id: string]: bibtex.Entry} = {};
-  for (const entry of bib.entries) bibliography[entry.key] = entry;
+
+  buf.push('<ul>');
+  for (const entry of bib.entries) {
+    // TODO: improve citation format
+    const title = `<em>${entry.fields.title[0]}</em>`;
+    const date = entry.fields.year[0];
+    buf.push(`<li><a href="${entry.fields.url[0]}">${title}</a> — ${date}</li>`);
+  }
+  buf.push('</ul>');
 
   return {
     title: 'Research | pkmn.ai',
     header: '<h2>Research</h2>',
-    content: '<p>TODO</p>',
+    content: `<div id="research">${buf.join('')}</div>`,
     edit: 'https://github.com/pkmn/ai/edit/main/src/site/research.bib',
   };
 }
