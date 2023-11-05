c2ce70b51a131f0b391a62b538154b7e0735df16
Kirk Scheibelhut <kjs@scheibo.com>
2023-11-04 18:21:32 -0700

    add URL aliases

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,3 @@
-// TODO
-// projects = papers/agents
-// leaderboard(s) = ladder(s)/tournament(s)/ranking(s)
-// glossary = terms/definitions/terminology
-// research = reading
-
 // /leaderboard
 //   - nav defaulting to - “Regulated — Open”, rules links in small head matter)
 //   - Open link needs third nav tracking multiple historical open battle periods
