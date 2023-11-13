95990770ba94c9c6da976cbb95314c540f49e22a
Kirk Scheibelhut <kjs@scheibo.com>
2023-11-12 17:55:14 -0800

    MDP takes a team
    in random battles it can read the team off the wire, but if no team
    is provided (and for opponents team) it has to assume some average
    EVs

--- src/example/max-damage-player.ts
+++ src/example/max-damage-player.ts
@@ -1,6 +1,15 @@
+import {PokemonSet} from '@pkmn/types';
+
 import {Choice, Player} from './player';
 
 export class MaxDamagePlayer extends Player {
+  private readonly team?: PokemonSet[];
+
+  constructor(team?: PokemonSet[]) {
+    super();
+    this.team = team;
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   choose(choices: Choice[][]): Choice[] {
     throw new Error('Method not implemented.');
