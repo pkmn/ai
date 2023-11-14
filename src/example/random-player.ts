e1524501c961547459153dfa2a54f2c4403893fb
Kirk Scheibelhut <kjs@scheibo.com>
2023-11-13 18:20:13 -0800

    change defaults for RP

--- src/example/random-player.ts
+++ src/example/random-player.ts
@@ -4,29 +4,30 @@ export type Random = (min?: number, max?: number) => number;
 
 export namespace RandomPlayer {
   export interface Config {
-    switch?: {choose: number} | {consider: number};
-    mega?: {choose: number} | {consider: number};
-    zmove?: {choose: number} | {consider: number};
-    dynamax?: {choose: number} | {consider: number};
-    terastallize?: {choose: number} | {consider: number};
+    switch: {choose: number} | {consider: number};
+    mega: {choose: number} | {consider: number};
+    zmove: {choose: number} | {consider: number};
+    dynamax: {choose: number} | {consider: number};
+    terastallize: {choose: number} | {consider: number};
   }
 }
 
 export class RandomPlayer extends Player {
-  static CONFIG: RandomPlayer.Config = {
-    mega: {choose: 1},
-    zmove: {choose: 1},
-    dynamax: {choose: 1},
-    terastallize: {choose: 1},
-  };
-
   private readonly random: Random;
   private readonly config: RandomPlayer.Config;
 
-  constructor(random: Random, config?: RandomPlayer.Config) {
+  static CONFIG: RandomPlayer.Config = {
+    switch: {consider: 0},
+    mega: {choose: 1},
+    zmove: {consider: 1},
+    dynamax: {consider: 1},
+    terastallize: {consider: 1},
+  };
+
+  constructor(random: Random, config: Partial<RandomPlayer.Config> = {}) {
     super();
     this.random = random;
-    this.config = {...RandomPlayer.CONFIG, ...(config ?? {})};
+    this.config = {...RandomPlayer.CONFIG, ...config};
   }
 
   choose(choices: Choice[]): Choice {
@@ -63,15 +64,13 @@ export class RandomPlayer extends Player {
     for (const option of ['switch', 'mega', 'zmove', 'dynamax', 'terastallize'] as const) {
       if (partitioned[option].length) continue;
       const config = this.config[option];
-      if (config) {
-        if ('consider' in config) {
-          if (config.consider && (config.consider === 1 || this.random() < config.consider)) {
-            consider.push(...partitioned[option]);
-          }
-        } else {
-          if (config.choose && (config.choose === 1 || this.random() < config.choose)) {
-            return partitioned[option][this.random(partitioned[option].length)];
-          }
+      if ('consider' in config) {
+        if (config.consider && (config.consider === 1 || this.random() < config.consider)) {
+          consider.push(...partitioned[option]);
+        }
+      } else {
+        if (config.choose && (config.choose === 1 || this.random() < config.choose)) {
+          return partitioned[option][this.random(partitioned[option].length)];
         }
       }
     }
